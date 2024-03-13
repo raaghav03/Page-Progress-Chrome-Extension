83109e9d260f3ee00b4b5cfce5b7df5bc0e69570
raaghav03 <raghavnagpal2003@gmail.com>
2024-03-14 00:13:10 +0530

    color change for range

--- script.js
+++ script.js
@@ -12,6 +12,36 @@ function updateScrollPercentage() {
   const pageHeight = document.body.scrollHeight - windowHeight; // subtracting windowHeight to find scrollable height
   let scroll = pageHeight > 0 ? (windowScroll / pageHeight) * 100 : 100; // setting scroll percentage to 0 is scroll position is 0
   scroll = Math.min(scroll, 100);
+  if (scroll > 0 && scroll <= 25) {
+    percentageDisplay.classList.add("bg-color-1");
+    percentageDisplay.classList.remove(
+      "bg-color-2",
+      "bg-color-3",
+      "bg-color-4"
+    );
+  } else if (scroll > 25 && scroll <= 50) {
+    percentageDisplay.classList.add("bg-color-2");
+    percentageDisplay.classList.remove(
+      "bg-color-1",
+      "bg-color-3",
+      "bg-color-4"
+    );
+  } else if (scroll > 50 && scroll <= 75) {
+    percentageDisplay.classList.add("bg-color-3");
+    percentageDisplay.classList.remove(
+      "bg-color-1",
+      "bg-color-2",
+      "bg-color-4"
+    );
+  } else if (scroll > 75 && scroll <= 100) {
+    percentageDisplay.classList.add("bg-color-4");
+    percentageDisplay.classList.remove(
+      "bg-color-1",
+      "bg-color-2",
+      "bg-color-3"
+    );
+  }
+
   let scrollPercentage = `${scroll.toFixed(0)}%`; // converting it zero decimal place
   percentageDisplay.textContent = scrollPercentage;
 }
